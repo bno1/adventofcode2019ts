9edae6d0e4c00ac0de8c564b469d513a875fb7b4
bno1 <ealex95@gmail.com>
2019-12-24 16:36:14 +0200

    Update Day 08 to use Array2

--- src/d08.ts
+++ src/d08.ts
@@ -1,58 +1,20 @@
 import {ChallengeFromFile} from "./challenge";
 import {ChallengeRegistry} from "./challenge_registry";
+import {Array2} from "./array2";
 
+
+type Layer = Array2<Uint8Array>;
 
 const IMAGE_WIDTH = 25;
 const IMAGE_HEIGHT = 6;
 const IMAGE_PIXELS = IMAGE_WIDTH * IMAGE_HEIGHT;
 
-class Layer {
-  pixels: Uint8Array;
-  width: number;
-  height: number;
-
-  constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
-
-    this.pixels = new Uint8Array(this.width * this.height);
-  }
-
-  get(x: number, y: number): number {
-    return this.pixels[x + y * this.width];
-  }
-
-  set(x: number, y: number, v: number) {
-    this.pixels[x + y * this.width] = v;
-  }
-
-  setArray(arr: number[]) {
-    if (this.pixels.length != arr.length) {
-      throw new Error(
-        `Array length ${arr.length} differs from layer size` +
-        `${this.width}x${this.height}`);
-    }
-
-    this.pixels.set(arr);
-  }
-
-  rows(): Uint8Array[] {
-    let rows: Uint8Array[] = [];
-
-    for (let i = 0; i < this.height; i++) {
-      rows.push(this.pixels.slice(i * this.width, (i+1) * this.width));
-    }
-
-    return rows;
-  }
-}
-
 function readLayers(pixels: number[]): Layer[] {
   let layers: Layer[] = [];
 
   for (let i = 0; i < pixels.length; i += IMAGE_PIXELS) {
-    let layer = new Layer(IMAGE_WIDTH, IMAGE_HEIGHT);
-    layer.setArray(pixels.slice(i, i + IMAGE_PIXELS));
+    let layer = new Array2(IMAGE_WIDTH, IMAGE_HEIGHT, Uint8Array);
+    layer.set(pixels.slice(i, i + IMAGE_PIXELS));
     layers.push(layer);
   }
 
@@ -76,7 +38,7 @@ class ChallengeD08 extends ChallengeFromFile {
     for (let layer of layers) {
       let counts: {[key: number]: number} = {0: 0, 1: 0, 2: 0};
 
-      layer.pixels.forEach((v) => counts[v]++);
+      layer.forEach((v) => counts[v]++);
       if (counts[0] < best_counts[0]) {
         best_counts = counts;
       }
@@ -89,16 +51,16 @@ class ChallengeD08 extends ChallengeFromFile {
     let input = this.getInput();
     let layers = readLayers(input);
 
-    let render = new Layer(IMAGE_WIDTH, IMAGE_HEIGHT);
-    render.pixels.fill(2);
+    let render = new Array2(IMAGE_WIDTH, IMAGE_HEIGHT, Uint8Array);
+    render.fill(2);
 
     for (let layer of layers) {
       for (let y = 0; y < render.height; y++) {
         for (let x = 0; x < render.width; x++) {
-          if (render.get(x, y) == 2) {
-            const v = layer.get(x, y);
+          if (render.read(x, y) == 2) {
+            const v = layer.read(x, y);
             if (v != 2) {
-              render.set(x, y, v);
+              render.write(x, y, v);
             }
           }
         }
